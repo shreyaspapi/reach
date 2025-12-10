eb8feb384bf3b54992736c0e7852ab4bbf39beee
Shreyas Papinwar <spapinwar@gmail.com>
2025-12-10 13:30:58 +0530

    fixing yarn problem

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['@farcaster/auth-kit', '@farcaster/miniapp-sdk'],
+  transpilePackages: [
+    '@farcaster/auth-kit',
+    '@farcaster/auth-client', 
+    '@farcaster/miniapp-sdk',
+    '@farcaster/quick-auth',
+    'viem',
+  ],
   serverExternalPackages: [
     'pino',
     'thread-stream',
     '@walletconnect/ethereum-provider',
   ],
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
   turbopack: {},
 };
 
